6c667b54a59793f810c90a7273bf833b7c1dd36a
N-Epaphras <epaphrasnasasira21@gmail.com>
2024-09-20 19:57:03 +0300

    Add files via upload

---
+++ Nasasira Epaphras.php
@@ -0,0 +1,9 @@
+<?php
+$name="Nasasira Epaphras";
+$yearOfBirth=1990;
+$address="plot 78,main street,kabale-katuna road";
+$today=2024;
+$age= $today-$yearOfBirth;
+
+echo "welcome home ","$name, " ," $address.","you are"," $age " ,"years old.";
+?>
